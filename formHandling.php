369fa10f842d4fcf2b0c699aa489e317db8042de
HannaNash233 <145599518+HannaNash233@users.noreply.github.com>
2024-06-11 03:16:55 +0000

    Modified some things and added more pictures

--- formHandling.php
+++ formHandling.php
@@ -1,24 +1,30 @@
-<html>
+ <!-- <html>
   <head>
     <title>PHP Test</title>
   </head>
   <body>
+
    <?php
-    
-    if($_SERVER["REQUEST_METHOD"] === "POST"){
-      $firstName = test_input($_POST["fname"]);
-      $lastName = test_input($_POST["lname"]);
-      $email = test_input($_POST["email"]);
-        }
 
-    function test_input($data){
-      $data = trim($data);
-      $data = stripslashes($data);
-      $data = htmlspecialchars($data);
-    }
-    /* Works! No security still but trying to create php without the site leaving  */
-    echo <p>'Form created successfully!'</p>;
-    ?>
-    
+$servername = "localhost";
+$username = "root";
+$password = "";
 
-</html>
+$conn = new mysqli($servername, $username, $password);
+
+if($conn->connect_error){
+  die("Connection failed: " . conn->connect_error);
+}
+echo "Connected successfully";
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+  $fname = $_POST["fname"];
+  $lname = $_POST["lname"];
+  $email = $_POST["email"];
+}
+conn->close();
+?>
+
+  </body>
+</html> -->
+7
